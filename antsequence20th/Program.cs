edb3f8d49734964baa5148d4fe0755420d608cfd
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-05-12 20:30:07 +0900

    n번 째 개미수열 구하기

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,28 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
-        }
+            int end = 20;
+            string s = "1";// 1에서 시작
+            for(int i = 1; i <= end; i++)
+            {
+                Console.WriteLine(i + "번 째 수열: " + s); //1, 11, 21
+                var temp = ""; // 저장할 공간
+                var count = 0;
+                char c = s[0];
+                for(int j = 0; j < s.Length; j++)
+                {
+                    if (c == s[j]) count++;
+                    else 
+                    {
+                        temp += "" + c + count;
+                        count = 0;
+                        c = s[j];
+                        count++; //2가 1개
+                    }
+                    if (j + 1 == s.Length) temp += ""+c + count; //""는 값이 섞이는거... 방지하는 듯
+                }
+                s = temp; //temp에 있던 것을 s로 옮김
+            }
+        }// main
     }
 }
